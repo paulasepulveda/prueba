250f7801c3ce54df4cf346750855c308863e4acb
Paula Sepulveda <psepulveda26@gmail.com>
2016-11-11 12:58:12 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function agregarMensaje(){
+	var contacto = document.querySelector('.avatar');
+
+	var respuesta = document.createElement('div');
+	
+	var imagen = document.createElement('img');
+	var nombre = document.createElement('h4');
+	var mensaje = document.createElement('p');
+	var hora = document.createElement('text');
+
+	var nodoimagen = document.createTextNode()
+
+
+}
